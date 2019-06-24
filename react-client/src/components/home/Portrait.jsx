6f3a067f2018d935c95b9ba113c9efd11755d78f
wenhuangzeng <36428519+wenhuangzeng@users.noreply.github.com>
2019-06-23 23:39:29 -0400

    reset

--- react-client/src/components/home/Portrait.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Portrait.css';
-
-export default function AboutMe() {
-    return (
-        <div className="portrait">
-        </div>
-    )
-}
--- react-client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import './styles/normalize.css';
-
-ReactDOM.render(<App />, document.getElementById('app'));   
