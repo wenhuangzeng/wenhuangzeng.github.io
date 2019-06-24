cc68c728ec1e7785911cabc0957aa840e5e352a1
Wenhuang Zeng <wzeng4@itsjnj.com>
2019-06-24 12:14:33 -0400

    remove unneeded folders

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/wenhuangzeng/Desktop/wenhuangzeng/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
-      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
-    },{
-      plugin: require('/Users/wenhuangzeng/Desktop/wenhuangzeng/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
