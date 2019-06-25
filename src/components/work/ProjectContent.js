3069b4863c38ceff350e566cdee90454aed8e1e8
Wenhuang Zeng <wzeng4@itsjnj.com>
2019-06-25 17:11:59 -0400

    project card update

---
+++ src/components/work/ProjectContent.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        "name": "Pacman",
+        "image" : require('../../images/1.jpg'),
+        "tools" : ['Java', 'JavaFX'],
+        "description" : "An implementation of iconic game Pacman",
+        "details" : ["Utilized OOP to categorize components of Pacman using Java and JavaFX", "Implemented breadth-first search to control Ghosts’ movement"],
+        "links" : [
+            { "name" : "Github", "url" : "https://github.com" },
+            { "name" : "Demo", "url" : "https://google.com" }
+        ]
+        
+    }
+]
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,52 +1,96 @@
 import React from 'react';
+import { Helmet } from "react-helmet"
 import ProjectCard from '../components/work/ProjectCard';
-import { Grid, GridRow, GridColumn } from 'semantic-ui-react';
+import { Grid, GridRow, GridColumn, Container } from 'semantic-ui-react';
+import styles from './projects.module.scss';
+import projects from '../components/work/ProjectContent';
 
 const ProjectsPage = () => (
+    <div className={styles.ProjectsContainer}>
+        <Helmet>
+            <title>W | Z | Projects</title>
+            <meta name="description" content="Hi there!" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="author" content="Wenhuang Zeng" />
+            <meta name="keywords" content="Wenhuang Zeng, Brown University, Software Developer, Projects" />
+        </Helmet>
 
-    <Grid stackable>
-        <Grid.Row columns={4}>
-            <Grid.Column>
-                <ProjectCard 
-                    name={"whatt"}
-                    description={"description123"} 
-                    image={require('../images/1.jpg')}
-                    tools={['tool1', 'tool2']}
-                />
-            </Grid.Column>
+        <Container fluid className={styles.Heading}>
+        <h1><mark>___________</mark></h1>
+        </Container>
+
+        <Grid stackable>
+            <Grid.Row columns={5}>
+
+                {projects.map( (project) => {
+                    return (
+                        <Grid.Column>
+                            <ProjectCard project={project} />
+                        </Grid.Column>
+                    )
+
+                })}
+
+                {/* <Grid.Column>
+                    <ProjectCard 
+                        name={"whatt"}
+                        description={"description123"} 
+                        image={require('../images/1.jpg')}
+                        tools={['tool1', 'tool2']}
+                    />
+                </Grid.Column>
 
 
-            <GridColumn>
-                <ProjectCard 
-                    name={"whatt"}
-                    description={"description123"} 
-                    image={require('../images/2.jpg')}
-                    tools={['tool1', 'tool2']}
-                />
-            </GridColumn>
+                <GridColumn>
+                    <ProjectCard 
+                        name={"whatt"}
+                        description={"description123"} 
+                        image={require('../images/2.jpg')}
+                        tools={['tool1', 'tool2']}
+                    />
+                </GridColumn>
 
-            <Grid.Column>
-                <ProjectCard 
-                    name={"whatt"}
-                    description={"description123"} 
-                    image={require('../images/3.jpg')}
-                    tools={['tool1', 'tool2']}
-                />
-            </Grid.Column>
+                <Grid.Column>
+                    <ProjectCard 
+                        name={"whatt"}
+                        description={"description123"} 
+                        image={require('../images/3.jpg')}
+                        tools={['tool1', 'tool2']}
+                    />
+                </Grid.Column>
 
 
-            <Grid.Column>
-                <ProjectCard 
-                    name={"whatt"}
-                    description={"description123"} 
-                    image={require('../images/4.jpg')}
-                    tools={['tool1', 'tool2']}
-                />
-            </Grid.Column>
+                <Grid.Column>
+                    <ProjectCard 
+                        name={"whatt"}
+                        description={"description123"} 
+                        image={require('../images/4.jpg')}
+                        tools={['tool1', 'tool2']}
+                    />
+                </Grid.Column>
 
-        </Grid.Row>
+                <Grid.Column>
+                    <ProjectCard 
+                        name={"whatt"}
+                        description={"description123"} 
+                        image={require('../images/4.jpg')}
+                        tools={['tool1', 'tool2']}
+                    />
+                </Grid.Column>
 
-    </Grid>
+                <Grid.Column>
+                    <ProjectCard 
+                        name={"whatt"}
+                        description={"description123"} 
+                        image={require('../images/4.jpg')}
+                        tools={['tool1', 'tool2']}
+                    />
+                </Grid.Column> */}
+
+            </Grid.Row>
+
+        </Grid>
+    </div>
 )
 
 export default ProjectsPage
