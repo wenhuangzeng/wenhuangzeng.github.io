1d6e46e3deae13920e952b583097978130ba8587
Wenhuang Zeng <wenhuang_zeng@brown.edu>
2019-07-22 22:05:52 -0400

    trying to deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,17 +24,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/5.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/portrait/5.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-less`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      }
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
